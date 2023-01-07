74cd0dcd51b8640c7ad67a915932ecb902f8b448
abbas ejlali <abbas.ejlali18@gmail.com>
2023-01-07 07:18:10 -0800

    edit details.js components

--- src/components/Shaired/DetailsProduct.js
+++ src/components/Shaired/DetailsProduct.js
@@ -9,18 +9,18 @@ const DetailsProduct = (props) => {
   const id1 = props.match.params.id;
   const data = useContext(Products);
   const productmain = data[id1 - 1];
-  const { image, description, title, category, price } = productmain;
+  // const { image, description, title, category, price } = productmain;
 
   return (
     <div>
       <div>
-        <img src={image} alt="img" />
+        <img src={productmain.image} alt="img" />
         <div>
-          <span>{category}</span>
-          <h1>{title}</h1>
-          <p>{description}</p>
-          <span>{price}</span>
-          <Link to={Shop}>Back to Products</Link>
+          <span>{productmain.category}</span>
+          <h1>{productmain.title}</h1>
+          <p>{productmain.description}</p>
+          <span>{productmain.price}</span>
+          <Link to="/products">Back to Products</Link>
         </div>
       </div>
     </div>
